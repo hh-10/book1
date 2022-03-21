2b77dfacea1a2dec63725bbd0320c202a141bfcd
peace <hwahye.oh@gmail.com>
2022-03-22 08:38:26 +0900

    스프링에서 생성자를 통해 Bean을 주입하는 방식

--- src/main/java/book/springboot/service/posts/PostsService.java
+++ src/main/java/book/springboot/service/posts/PostsService.java
@@ -10,6 +10,14 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+/*
+스프링에서 Bean을 주입하는 방식
+@Autowired, setter, 생성자
+        생성자로 Bean 객체를 받도록 하면 @Autowired와 동일한 효과를 볼 수 있다.
+        final이 선언된 모든 필드를 인자값으로 하는 생성자를 롬복의 @RequiredArgsConstructor가 대신 생성해준다.
+        생성자를 직접 안 쓰고 롬복 어노테이션을 사용한 이유는 해당 클래스의 의존성 관계가 변경될 때마다 생성자 코드를 수정하는 번거로움을 해결하기 위함이다.
+
+ */
 @RequiredArgsConstructor
 @Service
 public class PostsService {
